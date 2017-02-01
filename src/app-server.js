6071f3dfba92b741de0c1e633d6c39517d577e81
Erik Jönsson <efjonsson@gmail.com>
2017-01-31 22:23:03 -0500

    add node-sass

--- src/app-server.js
+++ src/app-server.js
@@ -11,7 +11,6 @@ import { match, RouterContext } from 'react-router';
 import routes from './config/routes';
 import NotFoundPage from './components/NotFound';
 
-// initialize the server and configure support for ejs templates
 const app = new Express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,36 +20,25 @@ app.use(logger('dev'));
 
 app.use('/api', apiRouter);
 
-// define the folder that will be used for static assets
 app.use(Express.static(path.join(__dirname, 'static')));
 
 app.get('*', (req, res) => {
   match(
     { routes, location: req.url },
     (err, redirectLocation, renderProps) => {
-
-      // in case of error display the error message
       if (err) {
         return res.status(500).send(err.message);
       }
-
-      // in case of redirect propagate the redirect to the browser
       if (redirectLocation) {
         return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
       }
-
-      // generate the React markup for the current route
       let markup;
       if (renderProps) {
-        // if the current route matched we have renderProps
         markup = renderToString(<RouterContext {...renderProps} />);
       } else {
-        // otherwise we can render a 404 page
         markup = renderToString(<NotFoundPage />);
         res.status(404);
       }
-
-      // render the index template with the embedded React markup
       return res.render('index', { markup });
     },
   );
