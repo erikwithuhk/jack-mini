75ee6aba47a8ac5d236b74b58bc35ae7e9cb33cd
Erik Jönsson <efjonsson@gmail.com>
2017-03-20 23:34:56 -0400

    add timestamps to users

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,7 +1,9 @@
 class User {
-  constructor({ id, email }) {
+  constructor({ id, email, created_at, updated_at }) {
     this.id = id;
     this.email = email;
+    this.createdAt = created_at;
+    this.updatedAt = updated_at;
   }
 }
 
--- src/db/seeds/users.sql
+++ src/db/seeds/users.sql
@@ -1,4 +1,4 @@
 \c jack_mini
 
-INSERT INTO users (email) VALUES ('test@test.com');
-INSERT INTO users (email) VALUES ('erik@test.com');
+INSERT INTO users (email, created_at, updated_at) VALUES ('test@test.com', '1999-01-08 04:05:06 -8:00', '1999-01-08 04:05:06 -8:00');
+INSERT INTO users (email, created_at, updated_at) VALUES ('erik@test.com', '1999-01-08 04:05:06 -8:00', '1999-01-08 04:05:06 -8:00');
