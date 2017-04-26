3d23e33b7e8943182003e9360cde87393bf3f587
erikwithuhk <efjonsson@gmail.com>
2017-04-26 11:26:44 -0400

    fix linting errors

--- src/server.js
+++ src/server.js
@@ -1,15 +1,13 @@
-import path from 'path';
 import { Server } from 'http';
 import app from './app-server';
 
-const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'dev';
 
 const server = new Server(app);
 
-server.listen(port, err => {
+server.listen((port = process.env.PORT || 3000, err) => {
   if (err) {
     return console.error(err);
   }
-  console.info(`Server running on http://localhost:${port} [${env}]`);
+  return console.info(`Server running on http://localhost:${port} [${env}]`);
 });
